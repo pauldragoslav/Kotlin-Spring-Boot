fb1eb4f157875fbc3bd88608c854aa62ddc3b2a8
pauld <paul.dragoslav@gmail.com>
2020-01-13 20:33:56 +0000

    Upgrade to Spring-boot 2.2 and JUnit 5

--- src/test/kotlin/com/example/paul/controller/PersonControllerTest.kt
+++ src/test/kotlin/com/example/paul/controller/PersonControllerTest.kt
@@ -2,19 +2,16 @@ package com.example.paul.controller
 
 import com.example.paul.model.Gender
 import com.example.paul.model.Person
-import org.junit.Assert
-import org.junit.FixMethodOrder
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.MethodSorters
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.MethodOrderer.Alphanumeric
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@TestMethodOrder(Alphanumeric::class)
 class PersonControllerTest {
 
     @Autowired
@@ -24,9 +21,9 @@ class PersonControllerTest {
     fun test1AddPerson() {
         var person = Person(null, "John Smith", 20, Gender.MALE)
         person = restTemplate.postForObject("/persons", person, Person::class.java)
-        Assert.assertNotNull(person)
-        Assert.assertNotNull(person.id)
-        Assert.assertEquals(1, person.id)
+        assertNotNull(person)
+        assertNotNull(person.id)
+        assertEquals(1, person.id)
     }
 
     @Test
@@ -34,14 +31,14 @@ class PersonControllerTest {
         var person = Person(1, "John Smith", 21, Gender.MALE)
         restTemplate.put("/persons", person)
         person = restTemplate.getForObject("/persons/{id}", Person::class.java, 1)
-        Assert.assertNotNull(person)
-        Assert.assertEquals(21, person.age)
+        assertNotNull(person)
+        assertEquals(21, person.age)
     }
 
     @Test
     fun test3DeletePerson() {
         restTemplate.delete("/persons/{id}", 1)
         val person = restTemplate.getForObject("/persons/{id}", Person::class.java, 1)
-        Assert.assertNull(person)
+        assertNull(person)
     }
 }
